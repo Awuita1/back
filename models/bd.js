b459c71f8e94a698fb2ecf3586df26909b8dbef1
Agustin Molina <franagus370@gmail.com>
2024-11-08 11:06:59 -0300

    tarea del modulo 5 unidad 4 añadida(con fallos)

---
+++ models/bd.js
@@ -0,0 +1,14 @@
+var util = require('util');
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    conectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    port: process.env.MYSQL_PORT
+})
+
+pool.query = util.promisify(pool.query);
+module.exports = pool;
